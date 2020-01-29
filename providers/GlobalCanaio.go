794f3489be7a8b805a648942bc2cbce85e29e8ee
Toni Vujevic <toni.v1988@gmail.com>
2020-01-29 14:11:24 +0000

    Feature/get parcel data

---
+++ providers/GlobalCanaio.go
@@ -0,0 +1,53 @@
+package providers
+
+import (
+	"encoding/json"
+	"fmt"
+	"golang.org/x/net/html"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+func GetGlobalCanaioData(parcelNumber string) (string, bool) {
+	/* test with:
+	LE571379316CN,
+	S00000111674824,
+	S00000111794186,
+	S00000111603104,
+	S00000110926105,
+	LE711833812CN,
+	UW309135048CN,
+	LVS10060000009698541,
+	S00000095458023,
+	S00000089577386,
+	LE590547286CN,
+	S00000075933024,
+	*/
+	urlString := fmt.Sprintf("http://global.cainiao.com/detail.htm?mailNoList=%s", parcelNumber)
+	resp, err := http.Get(urlString)
+	if err != nil {
+		return "", false
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	result := string(body)
+
+	re := regexp.MustCompile(`id="waybill_list_val_box">(.*)</textarea>`)
+	submatchall := re.FindAllStringSubmatch(result, -1)
+	for _, element := range submatchall {
+		unescaped := html.UnescapeString(element[1])
+
+		// Declared an empty interface
+		var result map[string]interface{}
+
+		// Unmarshal or Decode the JSON to the interface.
+		json.Unmarshal([]byte(unescaped), &result)
+
+		parcelData, _ := json.Marshal(result["data"].([]interface{})[0])
+
+		return string(parcelData), true
+	}
+
+	return "", false
+}
