b52f6ec2a3c63c7042f03fb9de456cbdab7e66b1
big-toni <toni.v1988@gmail.com>
2020-04-02 12:30:10 +0200

    return structured data on parcel endpoint

--- courier/global_canaio.go
+++ courier/global_canaio.go
@@ -3,28 +3,15 @@ package courier
 import (
 	"encoding/json"
 	"fmt"
-	"golang.org/x/net/html"
 	"io/ioutil"
 	"net/http"
 	"regexp"
+
+	"golang.org/x/net/html"
 )
 
 // GetGlobalCanaioData func
 func GetGlobalCanaioData(parcelNumber string) ([]byte, bool) {
-	/* test with:
-	LE571379316CN,
-	S00000111674824,
-	S00000111794186,
-	S00000111603104,
-	S00000110926105,
-	LE711833812CN,
-	UW309135048CN,
-	LVS10060000009698541,
-	S00000095458023,
-	S00000089577386,
-	LE590547286CN,
-	S00000075933024,
-	*/
 	urlString := fmt.Sprintf("http://global.cainiao.com/detail.htm?mailNoList=%s", parcelNumber)
 	resp, err := http.Get(urlString)
 	if err != nil {
--- service/parcel.go
+++ service/parcel.go
@@ -1,12 +1,69 @@
 package service
 
 import (
+	"encoding/json"
+	"strconv"
+
 	courier "../courier"
+	"github.com/mitchellh/mapstructure"
 )
+
+type trackingInfo struct {
+	Description string `json:"description" mapstructure:"desc"`
+	ID          string `json:"id"`
+	Status      string `json:"status"`
+	Time        string `json:"time"`
+	TimeZone    string `json:"timeZone"`
+}
+
+// parcelData struct
+type parcelData struct {
+	DestCountry        string         `json:"destCountry"`
+	LastUpdated        string         `json:"lastUpdated"`
+	LatestTrackingInfo trackingInfo   `json:"latestTrackingInfo"`
+	OriginCountry      string         `json:"originCountry"`
+	ShippingDaysCount  float64        `json:"shippingDaysCount"`
+	Status             string         `json:"status"`
+	StatusDescription  string         `json:"statusDescription"`
+	TrackingHistory    []trackingInfo `json:"trackingHistory"`
+	TrackingNumber     string         `json:"trackingNumber"`
+}
 
 // GetParcelData func
 func GetParcelData(parcelNumber string) ([]byte, bool) {
-	return courier.GetGlobalCanaioData(parcelNumber)
+	response, _ := courier.GetGlobalCanaioData(parcelNumber)
+
+	var result map[string]interface{}
+	json.Unmarshal(response, &result)
+
+	var parcelData parcelData
+
+	parcelData.DestCountry = result["destCountry"].(string)
+	parcelData.LastUpdated = result["cachedTime"].(string)
+	// mapstructure.Decode(result["latestTrackingInfo"].(map[string]interface{}), &parcelData.LatestTrackingInfo)
+	parcelData.OriginCountry = result["originCountry"].(string)
+	parcelData.ShippingDaysCount = result["shippingTime"].(float64)
+	parcelData.Status = result["status"].(string)
+	parcelData.StatusDescription = result["statusDesc"].(string)
+	mapstructure.Decode(result["section2"].(map[string]interface{})["detailList"], &parcelData.TrackingHistory)
+	parcelData.TrackingNumber = result["mailNo"].(string)
+
+	latestTrackingInfo := result["latestTrackingInfo"].(map[string]interface{})
+	parcelData.LatestTrackingInfo.Description = latestTrackingInfo["desc"].(string)
+	parcelData.LatestTrackingInfo.Status = latestTrackingInfo["status"].(string)
+	parcelData.LatestTrackingInfo.Time = latestTrackingInfo["time"].(string)
+	parcelData.LatestTrackingInfo.TimeZone = latestTrackingInfo["timeZone"].(string)
+
+	historyLen := len(parcelData.TrackingHistory)
+	parcelData.LatestTrackingInfo.ID = strconv.Itoa(historyLen - 1)
+
+	// Add indexes in reversed order
+	for i := range parcelData.TrackingHistory {
+		parcelData.TrackingHistory[historyLen-1-i].ID = strconv.Itoa(i)
+	}
+
+	json, _ := json.Marshal(parcelData)
+	return json, true
 }
 
 // ResolveCourier func
