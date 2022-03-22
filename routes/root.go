c9f12d8875bccd25a504c388f70b861b91f00d89
big-toni <toni.v1988@gmail.com>
2022-03-22 09:11:34 +0100

    tidy

--- routes/root.go
+++ routes/root.go
@@ -8,5 +8,5 @@ import (
 // Root func
 func Root(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Parcels home page")
+	fmt.Fprintf(w, "Parcel Tracker home page")
 }
